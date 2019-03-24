f7cacc73011f00215a48f785abd0ae5ad138f039
NishuGoel <nishu0505@gmail.com>
2019-03-24 19:02:49 +0530

    Routing Module updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductComponent },
+	{ path: 'products', component: ProductComponent },
+	{ path: 'cart', component: CartComponent },
+	{ path: '**', redirectTo: '' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
